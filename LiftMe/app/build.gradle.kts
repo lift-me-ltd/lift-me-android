1de47bae61b61ca3f45162cbc3552ff5795563f2
Kalina Valeva <>
2024-06-03 16:56:36 +0300

    added ./gradlew detekt and text fonts

--- LiftMe/app/build.gradle.kts
+++ LiftMe/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.officegym.liftme"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -37,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
