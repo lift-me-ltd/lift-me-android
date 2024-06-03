81b42c538604071f26ae222fe65234953df94f5c
Kalina Valeva <>
2024-06-03 22:02:44 +0300

    detekt fixes

--- LiftMe/app/src/main/java/com/officegym/liftme/MainActivity.kt
+++ LiftMe/app/src/main/java/com/officegym/liftme/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.officegym.liftme.ui.theme.LiftMeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LiftMeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
-                }
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {}
             }
         }
     }
