42077a766cc6506b52ebd467693c508eefb3869f
Kalina Valeva <>
2024-08-16 19:41:34 +0300

    - primary button with arrow

--- LiftMe/app/src/main/java/com/officegym/liftme/ui/signup/SignUpUIComponents.kt
+++ LiftMe/app/src/main/java/com/officegym/liftme/ui/signup/SignUpUIComponents.kt
@@ -3,14 +3,12 @@ package com.officegym.liftme.ui.signup
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
--- LiftMe/app/src/main/java/com/officegym/liftme/ui_components/Buttons.kt
+++ LiftMe/app/src/main/java/com/officegym/liftme/ui_components/Buttons.kt
@@ -1,13 +1,23 @@
 package com.officegym.liftme.ui_components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.officegym.liftme.R
 import com.officegym.liftme.ui.text_styles.Text_md
 import com.officegym.liftme.ui.theme.LocalLMTheme
 
@@ -15,16 +25,25 @@ import com.officegym.liftme.ui.theme.LocalLMTheme
 fun ButtonWithArrow(
     text: String,
     modifier: Modifier = Modifier,
-    onClick: () -> Unit,
+    onClick: () -> Unit
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(12.dp))
             .background(LocalLMTheme.current.colors.white)
+            .clickable { onClick() }
     ) {
-        Row {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = dimensionResource(id = R.dimen.vertical_padding_button)),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Text_md(text = text, textColor = LocalLMTheme.current.colors.black)
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.spacing_xs)))
+            Image(painter = painterResource(id = R.drawable.arrow_right), contentDescription = "arrow")
         }
     }
 }
