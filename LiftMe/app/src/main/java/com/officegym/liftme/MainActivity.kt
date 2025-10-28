92926b4c524bd77e1c8437ff3ff7ff37667b4fae
Kalina Valeva <kalina.valevaa@gmail.com>
2025-10-28 21:26:56 +0200

    Remove unused imports

--- LiftMe/app/src/main/java/com/officegym/liftme/MainActivity.kt
+++ LiftMe/app/src/main/java/com/officegym/liftme/MainActivity.kt
@@ -1,24 +1,18 @@
 package com.officegym.liftme
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.officegym.liftme.navigation.LMNavHost
-import com.officegym.liftme.navigation.Navigator
-import com.officegym.liftme.navigation.model.Screen
 import com.officegym.liftme.ui.theme.LiftMeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
